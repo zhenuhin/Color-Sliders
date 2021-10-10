2613c2a9e69f6da2bdc851210fe7f9a4069d5a38
Alexandr <zhenuhin@inbox.ru>
2021-10-10 14:57:38 +0300

    It works

--- Color Sliders/ViewController.swift
+++ Color Sliders/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var colorView: UIView!
 
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 20
+        setupSliders(for: redSlider)
+        setupSliders(for: greenSlider)
+        setupSliders(for: blueSlider)
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
-
+    
+    @IBAction func redSliderMoved() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        changeViewColor()
+    }
+    @IBAction func greenSliderMoved() {
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        changeViewColor()
+    }
+    @IBAction func blueSliderMoved() {
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        changeViewColor()
+        
+    }
+    
+    
+    private func setupSliders(for slider: UISlider) {
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.value = 0
+    }
+    
+    private func changeViewColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
 
 }
 
