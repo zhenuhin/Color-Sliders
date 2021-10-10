4138c506f0fff83facbd9a226872855362cc9980
Alexandr <zhenuhin@inbox.ru>
2021-10-10 12:53:57 +0300

    Initial Commit

---
+++ Color Sliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Color Sliders
+//
+//  Created by Александр Женухин on 10.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
